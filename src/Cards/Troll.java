9dc04ecd46cf57c85faafae3260604264b8976d9
btcHehe <46330318+btcHehe@users.noreply.github.com>
2021-06-11 10:29:49 +0200

    Troll + Vampire cards

---
+++ src/Cards/Troll.java
@@ -0,0 +1,14 @@
+package Cards;
+
+public class Troll extends Card
+{
+    public Troll()
+    {
+
+    }
+
+    public void action()
+    {
+        System.out.println("le Problem?");
+    }
+}
---
+++ src/Cards/Vampire.java
@@ -0,0 +1,15 @@
+package Cards;
+
+public class Vampire extends Card
+{
+    public Vampire()
+    {
+
+    }
+
+    public void action()
+    {
+        System.out.println("*Sucks your blood/dick*");
+    }
+
+}
